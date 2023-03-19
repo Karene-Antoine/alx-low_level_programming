23e2e064a99f4cd696b1a8485522c80869f42188
Karene-Antoine <antoinekarene@gmail.com>
2023-03-19 15:32:10 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Prints
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar (i + '0');
+if (i == 9)
+{
+break;
+}
+putchar (',');
+putchar (' ');
+}
+return (0);
+}
